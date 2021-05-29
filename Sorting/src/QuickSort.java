6abf86347f1261d34a9f3f98a69b6da0da0ce917
iamRaman <ramanv6jan@gmail.com>
2021-05-30 02:13:42 +0530

    Quicksort to handle duplicates.

--- Sorting/src/QuickSort.java
+++ Sorting/src/QuickSort.java
@@ -6,31 +6,54 @@ public class QuickSort {
 		
 		int arr[] = {4,2,15,6,3,2,5,7,5,44,32,1,23};
 		
-		quickSort(arr,0,arr.length-1);
+		sort(arr,0,arr.length-1);
 		
 		for(int ele: arr) {
-			System.out.println(ele+" ");
-		}
-	}
-	
-	static void quickSort(int[] arr, int low, int high) {
-		
-		if( low < high ) {
-			
+			System.out.print(ele+" ");
 		}
 	}
 
-	
+	static void sort(int[] arr, int low, int high) {
+		
+		if (low < high) {
+			int pivot = arr[getPivot(low, high)];
+			int lt = low;
+			int gt = high;
+
+			int i = low;
+
+			while (i <= gt) {
+
+				if (arr[i] < pivot) {
+					exchange(i, lt, arr);
+					i++;
+					lt++;
+				}
+
+				else if (arr[i] > pivot) {
+					exchange(i, gt, arr);
+					gt--;
+				} else
+					i++;
+			}
+
+			sort(arr, low, lt - 1);
+			sort(arr, gt + 1, high);
+		}
+	}
 
 	static int getPivot(int low, int high) {
 		
 		Random rand = new Random();
-		int pivot = rand.nextInt(high-low) + low;
+		int pivot = rand.nextInt(high-low)+low;
 		
 		return pivot;
 	}
 	
 	static void exchange(int one, int two, int[] arr) {
+		
+		if(one == two)
+			return;
 		
 		arr[one] = arr[one] + arr[two];
 		arr[two] = arr[one] - arr[two];
