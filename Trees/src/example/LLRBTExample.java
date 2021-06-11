75a2cf2856751db108bc950f5976f56265626b98
iamRaman <ramanv6jan@gmail.com>
2021-06-11 22:18:18 +0530

    Left leaning red black tree find successor.

--- Trees/src/example/LLRBTExample.java
+++ Trees/src/example/LLRBTExample.java
@@ -1,6 +1,11 @@
 package example;
 
+import design.Node;
 import design.RedBlackTree;
+
+//        4
+//   2        7
+//1     3   5
 
 public class LLRBTExample {
 	public static void main(String[] args) {
@@ -13,6 +18,23 @@ public class LLRBTExample {
 			tree.insert(tree.root, item);
 		}
 		
-		tree.inorderTraversal(tree.root);
+		tree.inorderTraversal(tree.root); System.out.println();
+		System.out.println(findSuccessor(tree.root));
 	}
+	
+	public static Node findSuccessor(Node king) {
+		if( null == king || null == king.right ) {
+			return null;
+		}
+		
+		return giveMeMySuccessor(king.right);
+	}
+	
+	private static Node giveMeMySuccessor(Node prince) {
+		if(prince.left == null)
+			return prince;
+		
+		return giveMeMySuccessor(prince.left);
+	}
+	
 }
